566a6c71d343a05e4eee0938f46e330f0f68a3fe
HelinduS <helindusenadheera@gmail.com>
2026-05-01 14:47:25 +0530

    Downgrade targetSdk to 35 and remove XML header from misc.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.northstar"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
